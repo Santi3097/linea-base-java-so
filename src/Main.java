aad2248d8f2b24a7cb9498dae0d927f59db1fe27
Santi9730 <santiagoosorio214@gmail.com>
2023-08-30 17:59:46 -0500

    Clase Main con Hola mundo

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    protected String mensaje = "Juan";
+    public static void main(String[] args)
+    {
+        String mensaje = "Hola mundo x2";
+        System.out.println("mensaje");
+    }
+}
