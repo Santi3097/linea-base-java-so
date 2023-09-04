6f650f3b93d10421757f2595139f1b4a0e3d3f74
Santi9730 <santiagoosorio214@gmail.com>
2023-09-04 17:22:05 -0500

    Se crean clases de pokemon con atributos y metodos en JAVA

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
 public class Main {
-    protected String mensaje = "Juan";
-    public static void main(String[] args)
-    {
-        String mensaje = "Hola mundo x2";
-        System.out.println("mensaje");
+
+    public static void main(String[] args) {
+        Charmander charmanderUno = new Charmander(0.6, 8.5, "Macho", "Mar Llamas");
+        Bulbasaur bulbasaurUno = new Bulbasaur();
+
+        bulbasaurUno.setAltura(0.7);
+        bulbasaurUno.setPeso(6.9);
+        bulbasaurUno.setSexo("Macho");
+        bulbasaurUno.setHabilidad("Espesura");
+        bulbasaurUno.setDebilidades();
+
+        charmanderUno.ataqueMarLlamas(bulbasaurUno, -99);
+
+        System.out.println("Los puntos de vida restantes son: ");
+        System.out.println(bulbasaurUno.getPuntosVida());
+
     }
 }
